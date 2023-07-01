e4d551b8d2dd7699049a9f2d9f46fbc62bcbd4c4
Mitchell Woodin <mitchellwoodinart@gmail.com>
2023-07-01 13:32:48 +1000

    sets up admin site

--- project/settings.py
+++ project/settings.py
@@ -87,8 +87,11 @@ DATABASES = {
     }
 }
 
+# Auth
+
+AUTH_USER_MODEL = 'accounts.User'
+
 # Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
