a0aaacf9a495504d86b2d5ea3e7114ce0b2a1b17
Mitchell Woodin <mitchellwoodinart@gmail.com>
2023-07-04 14:57:42 +1000

    adds index doc string

--- open_budgie_ii/core/views.py
+++ open_budgie_ii/core/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 
 def index(request: HttpRequest) -> HttpResponse:
+    """
+    This is the entry point of the website
+    @param request: HttpRequest
+    @return: render of the index.html template
+    """
     context = {}
     return render(request, 'core/index.html', context)
