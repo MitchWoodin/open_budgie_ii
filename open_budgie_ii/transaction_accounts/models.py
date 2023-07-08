fbccb75487a7ac35746d3a075b03153c0d12db0a
Mitchell Woodin <mitchellwoodinart@gmail.com>
2023-07-08 16:49:43 +1000

    starts initial account modeling
    For #9

--- open_budgie_ii/transaction_accounts/models.py
+++ open_budgie_ii/transaction_accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class TransactionAccount(models.Model):
+    """
+    An account for transactions to be made.
+    """
+
+    name = models.CharField(max_length=100)
