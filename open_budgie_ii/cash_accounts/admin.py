b2ac904994182cebfd94e6d1039e5ea3450be79f
Mitchell Woodin <mitchellwoodinart@gmail.com>
2023-07-13 16:39:27 +1000

    adds cash_account admin page

--- open_budgie_ii/cash_accounts/admin.py
+++ open_budgie_ii/cash_accounts/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
+from .models import CashAccount
 
+
+@admin.register(CashAccount)
 class CashAccountAdmin(admin.ModelAdmin):
-    pass
+    list_display = ["name", "user", "current_balance"]
+    raw_id_fields = ["user"]
