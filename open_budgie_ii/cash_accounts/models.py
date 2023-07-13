0b94a4d201a9fb0a94d6b5780c904153a849a85d
Mitchell Woodin <mitchellwoodinart@gmail.com>
2023-07-13 16:39:09 +1000

    updates cash_account model

--- open_budgie_ii/cash_accounts/models.py
+++ open_budgie_ii/cash_accounts/models.py
@@ -7,7 +7,6 @@ class CashAccount(models.Model):
     """
 
     name = models.CharField(max_length=100)
-    transaction_type = models.CharField(max_length=100)
     initial_balance = models.FloatField()
     current_balance = models.FloatField()
     net_worth = models.BooleanField()
